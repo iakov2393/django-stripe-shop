68045cd5d52846354589d76d3fd9c3e214b52bd1
iakov2393 <iakov2393@gmail.com>
2026-04-27 10:45:50 +0300

    .

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 import stripe
-from django.http import HttpResponse, JsonResponse, Http404
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
